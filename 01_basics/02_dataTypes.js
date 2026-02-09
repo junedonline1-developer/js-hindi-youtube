2abeebe43f702a8d7e4c3911e04fc818d7823545
junedonline1-developer <juned.online1@gmail.com>
2026-02-09 13:48:53 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treart all JS code newer version 
+
+// alert(3 + 3) // We are using nodejs, Not browser
+
+console.log(3 
+    +
+     3) // code redability that should be high
+
+console.log("Juned")
+
+
+let name = "Juned"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "juned"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined 
